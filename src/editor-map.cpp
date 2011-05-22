4216b36f2f8ba114b8b2dffc4b1db87c8e79b09e
Adam Nielsen <malvineous@shikadi.net>
2011-05-22 12:49:09 +1000

    Update map editor
    Add zoom controls, grid lines, full palette support and ability to handle
    multiple tilesets.  Use libgamemaps functions to convert map codes into
    tile images.

--- src/editor-map.cpp
+++ src/editor-map.cpp
@@ -18,11 +18,14 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <map>
+#include <boost/shared_array.hpp>
 #include <camoto/gamemaps.hpp>
 #include <camoto/gamegraphics.hpp>
 #include <wx/imaglist.h>
 #include <wx/artprov.h>
 #include <wx/glcanvas.h>
+#include "main.hpp"
 #include "editor-map.hpp"
 
 #ifdef HAVE_GL_GL_H
@@ -52,13 +55,17 @@ class MapCanvas: public wxGLCanvas
 		std::vector<bool> visibleLayers;
 		bool viewportVisible;
 
-		MapCanvas(wxWindow *parent, Map2DPtr map, TilesetPtr tileset, int *attribList)
+		MapCanvas(wxWindow *parent, Map2DPtr map, VC_TILESET tileset, int *attribList)
 			throw () :
 				wxGLCanvas(parent, wxID_ANY, wxDefaultPosition,
 					wxDefaultSize, 0, wxEmptyString, attribList),
 				map(map),
-				tileset(tileset)
+				tileset(tileset),
+				zoomFactor(2),
+				gridVisible(false),
+				activeLayer(0)
 		{
+			assert(tileset.size() > 0);
 			// Initial state is all layers visible
 			int layerCount = map->getLayerCount();
 			for (int i = 0; i < layerCount; i++) {
@@ -72,60 +79,72 @@ class MapCanvas: public wxGLCanvas
 			glEnable(GL_TEXTURE_2D);
 			glDisable(GL_DEPTH_TEST);
 
-			const Tileset::VC_ENTRYPTR& tiles = tileset->getItems();
-			this->textureCount = tiles.size();
-			this->texture = new GLuint[this->textureCount];
-			glGenTextures(this->textureCount, this->texture);
-
-			glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-			GLushort r[256], g[256], b[256], a[256];;
-			if (tileset->getCaps() & Tileset::HasPalette) {
-				PaletteTablePtr pal = tileset->getPalette();
-				for (int i = 0; i < 256; i++) {
-					r[i] = ((*pal)[i].red << 8) | (*pal)[i].red;
-					g[i] = ((*pal)[i].green << 8) | (*pal)[i].green;
-					b[i] = ((*pal)[i].blue << 8) | (*pal)[i].blue;
-					a[i] = (GLushort)-1;
-				}
+			PaletteTablePtr palDefault;
+			if (tileset[0]->getCaps() & Tileset::HasPalette) {
+				palDefault = tileset[0]->getPalette();
+				assert(palDefault);
 			} else {
-				// Default palette
-				for (int i = 0; i < 256; i++) {
-					r[i] = (i & 4) ? ((i & 8) ? 0xFFFF : 0xAAAA) : ((i & 8) ? 0x5555 : 0x0000);
-					g[i] = (i & 2) ? ((i & 8) ? 0xFFFF : 0xAAAA) : ((i & 8) ? 0x5555 : 0x0000);
-					b[i] = (i & 1) ? ((i & 8) ? 0xFFFF : 0xAAAA) : ((i & 8) ? 0x5555 : 0x0000);
-					a[i] = (GLushort)-1;
+				// Load default palette
+				palDefault = createDefaultCGAPalette();
+			}
+
+			// TODO: Load unknown/default tile the same size (if possible) as the grid
+			GLuint unknownTile = 0;
+
+			for (int i = 0; i < layerCount; i++) {
+				Map2D::LayerPtr layer = this->map->getLayer(i);
+				Map2D::Layer::ItemPtrVectorPtr content = layer->getAllItems();
+				for (Map2D::Layer::ItemPtrVector::iterator c = content->begin(); c != content->end(); c++) {
+					GLuint t;
+					glGenTextures(1, &t);
+					glBindTexture(GL_TEXTURE_2D, t);
+					glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+					glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+					glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+					glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+					unsigned int code = (*c)->code;
+					if (this->textureMap.find(code) == this->textureMap.end()) {
+						// This tile code doesn't have an associated image yet
+						ImagePtr image = layer->imageFromCode(code, tileset);
+						if (image) {
+							// Got an image for this tile code
+
+							unsigned int width, height;
+							image->getDimensions(&width, &height);
+							if ((width > 512) || (height > 512)) continue; // image too large
+
+							PaletteTablePtr pal;
+							if (image->getCaps() & Image::HasPalette) {
+								pal = image->getPalette();
+							} else {
+								pal = palDefault;
+							}
+							assert(pal);
+							StdImageDataPtr data = image->toStandard();
+							StdImageDataPtr mask = image->toStandardMask();
+							boost::shared_array<uint32_t> combined(new uint32_t[width * height]);
+							uint8_t *d = data.get(), *m = mask.get();
+							uint8_t *c = (uint8_t *)combined.get();
+							for (int p = 0; p < width * height; p++) {
+								*c++ = *m & 0x01 ? 255 : (*pal)[*d].blue;
+								*c++ = *m & 0x01 ?   0 : (*pal)[*d].green;
+								*c++ = *m & 0x01 ? 255 : (*pal)[*d].red;
+								*c++ = *m & 0x01 ?   0 : (*pal)[*d].alpha;
+								m++; d++;
+							}
+
+							glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_BGRA, GL_UNSIGNED_BYTE, combined.get());
+							if (glGetError()) std::cerr << "[editor-tileset] GL error loading texture into id " << t << std::endl;
+						} else {
+							std::cerr << "[editor-map] Could not get an image for tile "
+								"code " << code << std::endl;
+							glDeleteTextures(1, &t);
+							t = unknownTile;
+						}
+						this->textureMap[code] = t;
+					}
 				}
 			}
-			glPixelTransferi(GL_MAP_COLOR, GL_TRUE);
-			glPixelMapusv(GL_PIXEL_MAP_I_TO_R, 256, r);
-			glPixelMapusv(GL_PIXEL_MAP_I_TO_G, 256, g);
-			glPixelMapusv(GL_PIXEL_MAP_I_TO_B, 256, b);
-			glPixelMapusv(GL_PIXEL_MAP_I_TO_A, 256, a);
-
-			GLuint *t = this->texture;
-			for (Tileset::VC_ENTRYPTR::const_iterator i = tiles.begin();
-				i != tiles.end(); i++, t++)
-			{
-				// Bind each texture in turn to the 2D target
-				glBindTexture(GL_TEXTURE_2D, *t);
-
-				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-
-				// Load an image into the 2D target, which will affect the texture
-				// previously bound to it.
-				ImagePtr image = tileset->openImage(*i);
-				StdImageDataPtr data = image->toStandard();
-				unsigned int width, height;
-				image->getDimensions(&width, &height);
-				if ((width > 512) || (height > 512)) continue; // image too large
-				// TODO: If this image has a palette, load it
-				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_COLOR_INDEX, GL_UNSIGNED_BYTE, data.get());
-				if (glGetError()) std::cerr << "[editor-tileset] GL error loading texture into id " << *t << std::endl;
-			}
-			glPixelTransferi(GL_MAP_COLOR, GL_FALSE);
 
 			this->glReset();
 		}
@@ -133,10 +152,36 @@ class MapCanvas: public wxGLCanvas
 		~MapCanvas()
 			throw ()
 		{
-			glDeleteTextures(this->textureCount, this->texture);
-			delete[] this->texture;
+			for (std::map<unsigned int, GLuint>::iterator i = this->textureMap.begin(); i != this->textureMap.end(); i++) {
+				glDeleteTextures(1, &i->second);
+			}
 		}
 
+		void setZoomFactor(int f)
+			throw ()
+		{
+			this->zoomFactor = f;
+			this->glReset();
+			this->redraw();
+			return;
+		}
+
+		void showGrid(bool visible)
+			throw ()
+		{
+			this->gridVisible = visible;
+			this->glReset();
+			this->redraw();
+			return;
+		}
+
+		void setActiveLayer(int layer)
+		{
+			this->activeLayer = layer;
+			this->glReset();
+			this->redraw();
+			return;
+		}
 
 		void onEraseBG(wxEraseEvent& ev)
 		{
@@ -164,7 +209,7 @@ class MapCanvas: public wxGLCanvas
 			glViewport(0, 0, s.x, s.y);
 			glMatrixMode(GL_PROJECTION);
 			glLoadIdentity();
-			glOrtho(0, s.x / 2, s.y / 2, 0, -1, 1);
+			glOrtho(0, s.x / this->zoomFactor, s.y / this->zoomFactor, 0, -1, 1);
 			glMatrixMode(GL_MODELVIEW);
 			glLoadIdentity();
 			return;
@@ -174,16 +219,17 @@ class MapCanvas: public wxGLCanvas
 		void redraw()
 			throw ()
 		{
-			wxSize s = this->GetClientSize();
 			this->SetCurrent();
 			glClearColor(0.5, 0.5, 0.5, 1);
 			glClear(GL_COLOR_BUFFER_BIT);
 
 			glEnable(GL_TEXTURE_2D);
 
-			glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
+			glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
 
-			int layerCount = map->getLayerCount();
+			glEnable(GL_BLEND);
+			glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+			int layerCount = this->map->getLayerCount();
 			for (int i = 0; i < layerCount; i++) {
 				if (this->visibleLayers[i]) {
 					Map2D::LayerPtr layer = this->map->getLayer(i);
@@ -203,23 +249,12 @@ class MapCanvas: public wxGLCanvas
 					}
 
 					int tileWidth, tileHeight;
-					if (layer->getCaps() & Map2D::Layer::HasOwnTileSize) {
-						layer->getTileSize(&tileWidth, &tileHeight);
-					} else {
-						// Use global tile size
-						if (this->map->getCaps() & Map2D::HasGlobalTileSize) {
-							this->map->getTileSize(&tileWidth, &tileHeight);
-						} else {
-							std::cout << "[editor-map] BUG: Layer uses map's tile size, but "
-								"map doesn't have a global tile size!\n";
-							continue;
-						}
-					}
+					this->getLayerTileSize(layer, &tileWidth, &tileHeight);
 
 					Map2D::Layer::ItemPtrVectorPtr content = layer->getAllItems();
 					for (Map2D::Layer::ItemPtrVector::iterator c = content->begin(); c != content->end(); c++) {
 
-						glBindTexture(GL_TEXTURE_2D, this->texture[(*c)->code]);
+						glBindTexture(GL_TEXTURE_2D, this->textureMap[(*c)->code]);
 						glBegin(GL_QUADS);
 						glTexCoord2d(0.0, 0.0);
 						glVertex2i( (*c)->x      * tileWidth,  (*c)->y      * tileHeight);
@@ -235,6 +270,25 @@ class MapCanvas: public wxGLCanvas
 			}
 
 			glDisable(GL_TEXTURE_2D);
+
+			if ((this->gridVisible) && (this->activeLayer >= 0)) {
+				Map2D::LayerPtr layer = this->map->getLayer(this->activeLayer);
+				int tileWidth, tileHeight;
+				this->getLayerTileSize(layer, &tileWidth, &tileHeight);
+				wxSize s = this->GetClientSize();
+
+				glColor4f(0.2, 0.2, 0.2, 0.35);
+				glBegin(GL_LINES);
+				for (int x = 0; x < s.x; x += tileWidth) {
+					glVertex2i(x, 0);
+					glVertex2i(x, s.y);
+				}
+				for (int y = 0; y < s.y; y += tileHeight) {
+					glVertex2i(0,   y);
+					glVertex2i(s.x, y);
+				}
+				glEnd();
+			}
 
 			if (this->viewportVisible) {
 				int vpX, vpY;
@@ -252,12 +306,62 @@ class MapCanvas: public wxGLCanvas
 			return;
 		}
 
+		void getLayerTileSize(Map2D::LayerPtr layer, int *tileWidth, int *tileHeight)
+			throw ()
+		{
+			if (layer->getCaps() & Map2D::Layer::HasOwnTileSize) {
+				layer->getTileSize(tileWidth, tileHeight);
+			} else {
+				// Use global tile size
+				if (this->map->getCaps() & Map2D::HasGlobalTileSize) {
+					this->map->getTileSize(tileWidth, tileHeight);
+				} else {
+					std::cout << "[editor-map] BUG: Layer uses map's tile size, but "
+						"map doesn't have a global tile size!\n";
+					*tileWidth = *tileHeight = 64; // use some really weird value :-)
+					return;
+				}
+			}
+			return;
+		}
+
+		void onMouseMove(wxMouseEvent& ev)
+		{
+			int layerCount = this->map->getLayerCount();
+			std::cout << ev.m_x << "," << ev.m_y << " == ";
+			for (int i = 0; i < layerCount; i++) {
+				if (this->visibleLayers[i]) {
+					Map2D::LayerPtr layer = this->map->getLayer(i);
+
+					int tileWidth, tileHeight;
+					this->getLayerTileSize(layer, &tileWidth, &tileHeight);
+
+					Map2D::Layer::ItemPtrVectorPtr content = layer->getAllItems();
+					for (Map2D::Layer::ItemPtrVector::iterator c = content->begin(); c != content->end(); c++) {
+						int tileX = (*c)->x * tileWidth;
+						int tileY = (*c)->y * tileHeight;
+						if (
+							(tileX <= ev.m_x) && (tileX + tileWidth > ev.m_x) &&
+							(tileY <= ev.m_y) && (tileY + tileHeight > ev.m_y)
+						) {
+							std::cout << "l" << i << ": 0x" << std::hex << (*c)->code << std::dec << " ";
+						}
+					}
+				}
+			}
+			std::cout << "\n";
+			return;
+		}
+
 	protected:
 		Map2DPtr map;
-		TilesetPtr tileset;
+		VC_TILESET tileset;
 
-		int textureCount;
-		GLuint *texture;
+		std::map<unsigned int, GLuint> textureMap;
+
+		int zoomFactor;   ///< Zoom level (1 == 1:1, 2 == 2:1/doublesize, etc.)
+		bool gridVisible; ///< Draw a grid over the active layer?
+		int activeLayer;  ///< Index of layer currently selected in layer palette
 
 		DECLARE_EVENT_TABLE();
 
@@ -267,12 +371,13 @@ BEGIN_EVENT_TABLE(MapCanvas, wxGLCanvas)
 	EVT_PAINT(MapCanvas::onPaint)
 	EVT_ERASE_BACKGROUND(MapCanvas::onEraseBG)
 	EVT_SIZE(MapCanvas::onResize)
+	EVT_MOTION(MapCanvas::onMouseMove)
 END_EVENT_TABLE()
 
 class MapDocument: public IDocument
 {
 	public:
-		MapDocument(wxWindow *parent, Map2DPtr map, TilesetPtr tileset)
+		MapDocument(wxWindow *parent, Map2DPtr map, VC_TILESET tileset)
 			throw () :
 				IDocument(parent, _T("map")),
 				map(map),
@@ -281,19 +386,81 @@ class MapDocument: public IDocument
 			int attribList[] = {WX_GL_RGBA, WX_GL_DOUBLEBUFFER, WX_GL_DEPTH_SIZE, 0, 0};
 			this->canvas = new MapCanvas(this, map, tileset, attribList);
 
+			wxToolBar *tb = new wxToolBar(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTB_FLAT | wxTB_NODIVIDER);
+			tb->SetToolBitmapSize(wxSize(16, 16));
+
+			tb->AddTool(wxID_ZOOM_OUT, wxEmptyString,
+				wxImage(::path.guiIcons + _T("zoom_1-1.png"), wxBITMAP_TYPE_PNG),
+				wxNullBitmap, wxITEM_RADIO, _T("Zoom 1:1 (small)"),
+				_T("Set the zoom level so that one screen pixel is used for each tile pixel"));
+
+			tb->AddTool(wxID_ZOOM_100, wxEmptyString,
+				wxImage(::path.guiIcons + _T("zoom_2-1.png"), wxBITMAP_TYPE_PNG),
+				wxNullBitmap, wxITEM_RADIO, _T("Zoom 2:1 (normal)"),
+				_T("Set the zoom level so that two screen pixels are used for each tile pixel"));
+			tb->ToggleTool(wxID_ZOOM_100, true);
+
+			tb->AddTool(wxID_ZOOM_IN, wxEmptyString,
+				wxImage(::path.guiIcons + _T("zoom_3-1.png"), wxBITMAP_TYPE_PNG),
+				wxNullBitmap, wxITEM_RADIO, _T("Zoom 3:1 (big)"),
+				_T("Set the zoom level so that three screen pixels are used for each tile pixel"));
+
+			tb->AddSeparator();
+
+			tb->AddTool(IDC_TOGGLEGRID, wxEmptyString,
+				wxImage(::path.guiIcons + _T("grid.png"), wxBITMAP_TYPE_PNG),
+				wxNullBitmap, wxITEM_CHECK, _T("Show grid?"),
+				_T("Draw a grid to show where each tile is?"));
+
 			wxBoxSizer *s = new wxBoxSizer(wxVERTICAL);
+			s->Add(tb, 0, wxEXPAND);
 			s->Add(this->canvas, 1, wxEXPAND);
 			this->SetSizer(s);
+		}
+
+		void onZoomSmall(wxCommandEvent& ev)
+		{
+			this->canvas->setZoomFactor(1);
+			return;
+		}
+
+		void onZoomNormal(wxCommandEvent& ev)
+		{
+			this->canvas->setZoomFactor(2);
+			return;
+		}
+
+		void onZoomLarge(wxCommandEvent& ev)
+		{
+			this->canvas->setZoomFactor(4);
+			return;
+		}
+
+		void onToggleGrid(wxCommandEvent& ev)
+		{
+			this->canvas->showGrid(ev.IsChecked());
+			return;
 		}
 
 	protected:
 		MapCanvas *canvas;
 		Map2DPtr map;
-		TilesetPtr tileset;
+		VC_TILESET tileset;
 
 		friend class LayerPanel;
 
+		enum {
+			IDC_TOGGLEGRID = wxID_HIGHEST + 1,
+		};
+		DECLARE_EVENT_TABLE();
 };
+
+BEGIN_EVENT_TABLE(MapDocument, IDocument)
+	EVT_TOOL(wxID_ZOOM_OUT, MapDocument::onZoomSmall)
+	EVT_TOOL(wxID_ZOOM_100, MapDocument::onZoomNormal)
+	EVT_TOOL(wxID_ZOOM_IN, MapDocument::onZoomLarge)
+	EVT_TOOL(IDC_TOGGLEGRID, MapDocument::onToggleGrid)
+END_EVENT_TABLE()
 
 
 class LayerPanel: public IToolPanel
@@ -398,6 +565,8 @@ class LayerPanel: public IToolPanel
 
 		void onItemClick(wxListEvent& ev)
 		{
+			if (!this->doc) return;
+			this->doc->canvas->setActiveLayer(ev.GetData());
 			return;
 		}
 
@@ -534,13 +703,46 @@ IDocument *MapEditor::openObject(const wxString& typeMinor,
 		return NULL;
 	}
 
+	VC_TILESET tilesetVector;
+	tilesetVector.push_back(tileset);
+
+	// Open the second (masked tile) tileset if one was given
+	s = supp.find(_T("sprites"));
+	if (s != supp.end()) {
+		strGfxType = "tls-";
+		strGfxType.append(s->second.typeMinor.ToUTF8());
+
+		TilesetTypePtr ttp = gfxMgr->getTilesetTypeByCode(strGfxType);
+		if (!ttp) {
+			wxString wxtype(strGfxType.c_str(), wxConvUTF8);
+			wxString msg = wxString::Format(_T("Sorry, it is not possible to edit this "
+					"map as the \"%s\" tileset format is unsupported.  (No handler for \"%s\")"),
+				s->second.typeMinor.c_str(), wxtype.c_str());
+			wxMessageDialog dlg(this->parent, msg, _T("Open item"), wxOK | wxICON_ERROR);
+			dlg.ShowModal();
+			return NULL;
+		}
+
+		tileset = ttp->open(s->second.stream, NULL, suppData);
+		if (!tileset) {
+			wxString wxtype(strGfxType.c_str(), wxConvUTF8);
+			wxString msg = wxString::Format(_T("Tileset was rejected by \"%s\" handler"
+					" (wrong format?)"), wxtype.c_str());
+			wxMessageDialog dlg(this->parent, msg, _T("Open item"), wxOK | wxICON_ERROR);
+			dlg.ShowModal();
+			return NULL;
+		}
+		tilesetVector.push_back(tileset);
+	}
+
+
 	// Open the map file
 	MapPtr pMap(pMapType->open(data, suppData));
 	assert(pMap);
 
 	Map2DPtr map2d = boost::dynamic_pointer_cast<Map2D>(pMap);
 	if (map2d) {
-		return new MapDocument(this->parent, map2d, tileset);
+		return new MapDocument(this->parent, map2d, tilesetVector);
 	}
 
 	wxMessageDialog dlg(this->parent, _T("Sorry, this map is in a variant for "
