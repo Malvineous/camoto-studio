c490d31723a59c0fe30756d22828d7b2fbc0fa1b
Adam Nielsen <malvineous@shikadi.net>
2011-09-04 17:58:27 +1000

    Make autogen.sh return nonzero on failure

--- autogen.sh
+++ autogen.sh
@@ -3,5 +3,6 @@
 # Remove M4 files automatically copied (in case autotools version changes)
 rm -f m4/libtool.m4 m4/ltoptions.m4 m4/ltsugar.m4 m4/ltversion.m4 m4/lt~obsolete.m4
 
-autoreconf -fi;
-rm -Rf autom4te*.cache;
+autoreconf -fi || exit $?
+
+rm -Rf autom4te*.cache
