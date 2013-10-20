01810a9d6fbadca414b962eda95001b9405ef923
Adam Nielsen <malvineous@shikadi.net>
2013-10-20 11:17:48 +1000

    Map editor: Change texture map repeat flag to minimise visual artefacts

--- src/editor-map-canvas.cpp
+++ src/editor-map-canvas.cpp
@@ -319,8 +319,8 @@ void MapCanvas::loadTileImage(TEXTURE_MAP& tm, PaletteTablePtr& palDefault,
 		Texture t;
 		glGenTextures(1, &t.glid);
 		glBindTexture(GL_TEXTURE_2D, t.glid);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 
